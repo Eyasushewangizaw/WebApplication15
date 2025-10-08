584618b6aedb88eb7d07be023bf3d2f3323ccb71
Eyasushewangizaw <112728084+Eyasushewangizaw@users.noreply.github.com>
2025-10-08 17:00:08 -0400

    Update DB_connection.cs

--- WebApplication15/Models/DB_connection.cs
+++ WebApplication15/Models/DB_connection.cs
@@ -9,11 +9,7 @@ namespace WebApplication15.Models
     {
         public static SqliteConnection GetConnection()
         {
-            //Nebil's Path
-            //string connectionString = "Data Source= C:\\Users\\17065\\Documents\\SQLLite\\Northwind.db";
-
-            //Ubaid's Path
-            //string connectionString = "Data Source= C:\\Users\\Ubaid\\Documents\\483\\Sql Database\\Northwind.db";
+         
 
             string connectionString = "Data Source= C:\\Users\\15073\\OneDrive\\Desktop\\340\\Northwind.db";
 
